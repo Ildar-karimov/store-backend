68d2f85d84cc0a3ecf0cd52ff5e20a4f1aa63cb1
Ildar Karimov <karimovildar2000@gmail.com>
2022-05-15 22:51:49 +0300

    Add BrandDto

--- src/controllers/brandController.js
+++ src/controllers/brandController.js
@@ -1,21 +1,24 @@
 const {Brand, Product} = require('../models/models')
 const ApiError = require('../error/ApiError')
+const BrandDto = require("../dtos/brandDto");
 
 class BrandController {
-    async create(req, res) {
-        const {name} = req.body
-        const brand = await Brand.create({name})
-        return res.json(brand)
-    }
+  async create(req, res) {
+    const {name} = req.body
+    const brand = await Brand.create({name})
+    return res.json(brand)
+  }
 
-    async getAll(req, res) {
-        const brands = await Brand.findAll()
-        return res.json(brands)
-    }
+  async getAll(req, res) {
+    let brands = await Brand.findAll()
+    brands = brands.map(brand => new BrandDto(brand))
 
-    async getOne(req, res) {
+    return res.json(brands)
+  }
 
-    }
+  async getOne(req, res) {
+
+  }
 }
 
 module.exports = new BrandController()
---
+++ src/dtos/brandDto.js
@@ -0,0 +1,10 @@
+module.exports = class BrandDto {
+  id;
+  name;
+
+  constructor(props) {
+    this.id = props.id
+    this.name = props.name
+  }
+
+}
