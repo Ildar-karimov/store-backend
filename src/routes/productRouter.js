5a01bc0a577e0e35c7609ddbb6c75876294ddfd5
Ildar Karimov <karimovildar2000@gmail.com>
2022-06-09 22:14:22 +0300

    Minor

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,7 +2,6 @@ const Router = require('express')
 const router = new Router()
 const productController = require('../controllers/productController')
 const checkRole = require("../middleware/checkRoleMiddleware");
-// const checkAuth = require("../middleware/authMiddleware");
 
 router.post('/create',checkRole('ADMIN'), productController.create)
 router.get('/', productController.getAll)
