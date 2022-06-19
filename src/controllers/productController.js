2646b464011104a1a05cc9b4c442943fb0b1525d
Ildar Karimov <karimovildar2000@gmail.com>
2022-06-20 00:58:29 +0300

    Add getRecommendProducts method

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,12 +4,16 @@ const uuid = require('uuid')
 const path = require('path')
 const {Op} = require("sequelize");
 const productService = require('../services/productService')
+const UserDataset = require("../models/user-dataset-model");
+const User = require("../models/user-model");
+const orderModel = require("../models/order-model");
+const orderService = require("../services/orderService");
 
 class ProductController {
   async create(req, res, next) {
     try {
       let {name, price, count, additionalProducts, brandId, info} = req.body
-      additionalProducts =JSON.parse(additionalProducts)
+      additionalProducts = JSON.parse(additionalProducts)
       info = JSON.parse(info)
       const {img} = req.files
       let fileName = uuid.v4() + '.jpg'
@@ -52,6 +56,80 @@ class ProductController {
     const productData = await productService.getOne(id)
 
     return res.json(productData)
+  }
+
+  async getRecommendProducts(req, res) {
+    const {userDatasetId} = req.user
+
+    const dataset = await UserDataset.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+      where: {
+        id: {
+          [Op.not]: userDatasetId,
+        }
+      }
+    })
+
+    const currentUserDataset = await UserDataset.findOne({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'id'],
+      },
+      where: {id: userDatasetId}
+    })
+    // let rows = ['id', 'age', 'sex', 'categoryId', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8']
+
+    const currentUserDataset1 = currentUserDataset.toJSON()
+
+
+    let AlikePercent = dataset.map(row => {
+      const row1 = row.toJSON()
+      delete row1.id
+
+      let result = 0
+      Object.values(currentUserDataset1).forEach((val, i) => {
+        if (val === Object.values(row1)[i]) {
+          result++
+        }
+      })
+
+      return {
+        id: row.id,
+        alike: result / 10,
+      }
+    })
+
+    AlikePercent = AlikePercent
+    .filter(row => row.alike >= 0.6)
+    .sort((a, b) => b.alike - a.alike)
+
+    if (AlikePercent.length !== 0) {
+      let recommendProducts = []
+      for (const row of AlikePercent) {
+        let {id: userId} = await (await User.findOne({
+          attributes: ['id'],
+          where: {userDatasetId: row.id}
+        })).toJSON()
+
+        let userOrders = await orderModel.findAll({
+          where: {
+            userId,
+          },
+        })
+
+        for (const el of userOrders) {
+          let products = await orderService.getProductsOfOrder(el.id)
+          products.forEach(product => {
+            if (!recommendProducts.find(row => row.id === product.id)) {
+              recommendProducts.push(product)
+            }
+          })
+        }
+      }
+
+      return res.json(recommendProducts)
+    } else return res.json([])
   }
 }
 
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,9 +2,11 @@ const Router = require('express')
 const router = new Router()
 const productController = require('../controllers/productController')
 const checkRole = require("../middleware/checkRoleMiddleware");
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/create',checkRole('ADMIN'), productController.create)
 router.get('/', productController.getAll)
+router.get('/recommend-products', authMiddleware, productController.getRecommendProducts)
 router.get('/:id', productController.getOne)
 
 module.exports = router
